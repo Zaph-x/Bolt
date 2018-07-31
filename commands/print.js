0fe5d62c3177350291bb4fba61bf786b4cff4b80
Zaph <mikkel@luceartfoto.dk>
2018-07-31 19:19:32 +0200

    Initial commit! Making Bolt open source!

--- commands/print.js
+++
@@ -1,8 +0,0 @@
-
-module.exports.run = async message => {
-	if (message.member.hasPermission("MANAGE_MESSAGES")) {
-		message.delete(5);
-		let args = message.content.split(" ").slice(1);
-		message.channel.send(args.join(" "));
-	}
-};
